000ae1dd44d3d86d4ac8a365fb62e2f9681b5d73
Connor Runyan <connor.runyan495@gmail.com>
2022-12-01 08:53:01 -0700

    Day 1 - part 2

--- day1/solution.py
+++ day1/solution.py
@@ -2,7 +2,9 @@ def main():
     input_data = read_input("input.txt")
     groups = group_input(input_data)
     max_val = find_max(groups)
+    top_three = find_top_three(groups)
     print(max_val)
+    print(sum(top_three))
 
 
 def read_input(path: str) -> str:
@@ -14,6 +16,11 @@ def group_input(data: str) -> list[list[int]]:
     initial_groups = data.split("\n\n")
     sub_groups = [list(map(lambda v: int(v), g.split("\n"))) for g in initial_groups]
     return sub_groups
+
+def find_top_three(groups: list[list[int]]) -> list[int]:
+    sums = sorted([sum(grp) for grp in groups])
+    return sums[-3:]
+
 
 
 def find_max(groups: list[list[int]]) -> int:
